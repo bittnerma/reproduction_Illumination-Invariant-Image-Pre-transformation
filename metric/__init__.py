978b0b935df13518a1aa9955a692b50c5abe6ba0
M_Bit <mbittner.work@gmail.com>
2020-04-18 18:47:18 +0200

    More comment

--- metric/__init__.py
+++
@@ -1,6 +0,0 @@
-from .confusionmatrix import ConfusionMatrix
-from .iou import IoU
-from .accuracy import Accuracy
-from .metric import Metric
-
-__all__ = ['ConfusionMatrix', 'IoU', 'Metric','Accuracy']
